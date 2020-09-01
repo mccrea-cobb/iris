9496bcf16440d9cdb0576078eab4a5db4efe3fa6
McCrea Cobb <mccrea_cobb@fws.gov>
2020-09-01 10:56:30 -0800

    update read_iris, tables, and primr_report

--- R/read_iris.R
+++ R/read_iris.R
@@ -3,8 +3,6 @@
 #'
 #' @description Reads in a SQL table from the FWS IRIS Data Warehouse as a tibble
 #'
-#' @param region A character string representing the legacy FWS region of interest (e.g., Alaska = "FF07")
-#'
 #' @return A dataframe
 #' @export
 #'
@@ -18,14 +16,7 @@ read_iris <- function(schema,
   library(tidyverse)
   source("R/query_iris.R")
 
-  con <- dbConnect(odbc(),
-                   Driver = "ODBC Driver 17 for SQL Server",
-                   Server = "ifw9ecos-bvdb11.fws.doi.net\\ecos_beta",
-                   Database = "IRIS_DataWarehouse",
-                   Trusted_Connection = "yes",
-                   Port = 1433)
-
-  df <- query_iris(con, schema, tbl_name)
+  df <- query_iris(schema, tbl_name)
 
 
 }
