b91b617c9f60f0d8eac0194a67f39cb3226afa11
McCrea Cobb <mccrea_cobb@fws.gov>
2020-09-01 10:17:21 -0800

    update gitignore

--- R/read_iris.R
+++ R/read_iris.R
@@ -1,6 +1,6 @@
 
-#' Read a table from IRIS 
-#' 
+#' Read a table from IRIS
+#'
 #' @description Reads in a SQL table from the FWS IRIS Data Warehouse as a tibble
 #'
 #' @param region A character string representing the legacy FWS region of interest (e.g., Alaska = "FF07")
@@ -8,25 +8,24 @@
 #' @return A dataframe
 #' @export
 #'
-#' @examples read_iris("FF07")
+#' @examples read_iris("Refuges", "DimSurvey")
 
-read_iris <- function(con, 
-                      schema, 
+read_iris <- function(schema,
                       tbl_name){  # Make sure to VPN in first
-  
+
   library(DBI)
   library(odbc)
   library(tidyverse)
-  source("./code/functions/query_iris.R")
-  
+  source("R/query_iris.R")
+
   con <- dbConnect(odbc(),
                    Driver = "ODBC Driver 17 for SQL Server",
                    Server = "ifw9ecos-bvdb11.fws.doi.net\\ecos_beta",
                    Database = "IRIS_DataWarehouse",
                    Trusted_Connection = "yes",
                    Port = 1433)
-  
+
   df <- query_iris(con, schema, tbl_name)
-  
-  
+
+
 }
